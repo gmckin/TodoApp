e848e693e2e9f7440d2b9bc723f3f126900cd713
greg mckinney <gmck95@msn.com>
2016-10-31 10:14:23 -0400

    add initial commit

---
+++ ToDo.DataAccess/EfData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.DataAccess
+{
+  public class EfData
+  {
+
+    private TodoDBEntities1 db = new TodoDBEntities1();
+
+
+    public List<Todo> GetTodoList()
+    {
+      return db.Todoes.ToList();
+    }
+
+    public bool InsertTodo(Todo todo)
+    {
+      db.Todoes.Add(todo);
+
+      return db.SaveChanges() > 0;
+    }
+
+    public bool ChangeTodo(Todo todo)
+    {
+      var result = db.Todoes.SingleOrDefault(x => x.TodoId == todo.TodoId);
+
+      if (result != null)
+      {
+        if (todo.TodoId != 0)
+          result.TodoId = todo.TodoId;
+
+        if (todo.Description != null)
+          result.Description = todo.Description;
+
+        if (todo.Completed != true)
+          result.Completed = todo.Completed;
+      }
+      return db.SaveChanges() > 0;    
+    }
+
+    public bool DeleteTodo(Todo todo, int? id)
+    {
+      todo = db.Todoes.Where(t => t.TodoId == id).FirstOrDefault();
+      db.Todoes.Remove(todo);      
+
+      return db.SaveChanges() > 0;
+    }
+
+   
+    public bool MarkComplete(Todo todo, EntityState state)
+    {
+      var entry = db.Entry<Todo>(todo);
+      todo.Completed = true;
+      entry.State = state;
+
+      return db.SaveChanges() > 0;
+    }
+  }
+}
---
+++ TodoTests/EfTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Data;
+using System.Globalization;
+using System.Data.Entity;
+using ToDo.DataAccess;
+
+namespace TodoTests
+{
+  public class EfTests
+  {
+    private TodoDBEntities1 db = new TodoDBEntities1();
+    [Fact]
+    public void Test_InsertTodo()
+    {
+      EfData data = new EfData();
+      
+      var expected = new Todo() { Description = "Test Insert", Completed = false };
+
+      var actual = data.InsertTodo(expected);
+
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Test_GetTodoes()
+    {
+      var data = new EfData();
+      var actual = data.GetTodoList();
+
+      Assert.NotNull(actual);
+    }
+
+    [Fact]
+    public void Test_MarkCompleted()
+    {
+      var data = new EfData();
+      var expected = new Todo() { TodoId = 1, Description = "Create database", Completed = true };
+      var actual = data.MarkComplete(expected, System.Data.Entity.EntityState.Added);
+
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Test_DeleteTodo()
+    {
+      var id = 5;
+      var expected = db.Todoes.Where(x => x.TodoId == id).FirstOrDefault();
+      var data = new EfData();
+
+      var actual = data.DeleteTodo(expected, id);
+
+      Assert.True(actual);
+    }
+  }
+}
